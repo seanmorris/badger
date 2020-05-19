835134e27179ba717c54b4f23cb6c59450120c89
Sean Morris <sean@seanmorr.is>
2020-05-19 12:46:37 -0400

    README

---
+++ source/HomeView.php
@@ -0,0 +1,28 @@
+<?php
+namespace SeanMorris\Badger;
+class HomeView extends \SeanMorris\Theme\View
+{
+}
+
+__halt_compiler(); ?>
+<!DOCTYPE html>
+<head>
+<title>SeanMoris/Badger</title>
+<style type = "text/css">
+html,body{ height: 100%;}
+* {margin: 0px; padding: 0px;}
+body{
+display:         flex;
+flex-direction:  column;
+align-items:     center;
+justify-content: center;
+line-height:     1.25em;
+font-weight:     bold;
+font-family:     'Lucidia Console'
+}
+</style>
+</head>
+<body>
+	<p>MUSHROOM</p>
+	<p>MUSHROOM</p>
+</body>
